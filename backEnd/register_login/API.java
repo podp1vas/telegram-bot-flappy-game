19fd9bc099b81a97596c0e0c13c63dadbb26a42e
Liquida238 <megapoopuwu@gmail.com>
2025-07-03 23:50:06 +0300

    22

---
+++ backEnd/register_login/API.java
@@ -0,0 +1,7 @@
+package backEnd.register_login;
+
+public class API {
+    public static void api(String[] args) {
+        System.out.println("sadasdadsad");
+    }
+}
---
+++ main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import backEnd.register_login;
+
+public class Main {
+    public static void main(String[] args) {
+        API.api(args);
+    }
+}
